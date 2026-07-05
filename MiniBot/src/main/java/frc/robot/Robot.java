c56e9b62eb6e234713e7f70bb6398c242be17614
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2026-07-04 19:44:20 -0600

    Update Robot.java

--- MiniBot/src/main/java/frc/robot/Robot.java
+++ MiniBot/src/main/java/frc/robot/Robot.java
@@ -1,81 +1,3 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
-
-// package frc.robot;
-
-// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
-// import edu.wpi.first.cameraserver.CameraServer;
-// import edu.wpi.first.wpilibj.TimedRobot;
-// import edu.wpi.first.wpilibj.Timer;
-// import edu.wpi.first.wpilibj.XboxController;
-// import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
-// public class Robot extends TimedRobot {
-//   private final DifferentialDrive m_robotDrive;
-//   private final Timer timer;
-
-//   private final XboxController m_controller = new XboxController(0);
-
-//   private final WPI_TalonSRX leftFront = new WPI_TalonSRX(1);
-//   private final WPI_TalonSRX rightFront = new WPI_TalonSRX(2);
-//   private final WPI_TalonSRX leftBack = new WPI_TalonSRX(3);
-//   private final WPI_TalonSRX rightBack = new WPI_TalonSRX(4);
-
-//   /** Called once at the beginning of the robot program. */
-//   public Robot() {
-//     // We need to invert one side of the drivetrain so that positive voltages
-//     // result in both sides moving forward. Depending on how your robot's
-//     // gearbox is constructed, you might have to invert the left side instead.
-
-//     CameraServer.startAutomaticCapture();
-
-//     rightBack.follow(rightFront);
-//     leftBack.follow(leftFront);
-
-//     timer = new Timer();
-//     m_robotDrive = new DifferentialDrive(leftFront::set, rightFront::set);
-//   }
-
-//   @Override
-//   public void autonomousInit() {
-//     timer.reset();
-//     timer.start();
-//   }
-
-//   @Override
-//   public void autonomousPeriodic() {
-//     double time = timer.get();
-//     double cycleTime = 6.0;
-//     double phaseTime = time % cycleTime;
-
-//     if (phaseTime < 1.5) {
-//       // Move forward for 3 seconds
-//       leftFront.set(0.2); // Original was 0.4 * 0.5 = 0.2
-//       rightFront.set(0.2); // Original was 0.4 * 0.5 = 0.2
-//     } else if (phaseTime < 6.0) {
-//       // Move backward for next 3 seconds
-//       leftFront.set(-0.2); // Original was -0.4 * 0.5 = -0.2
-//       rightFront.set(-0.2); // Original was -0.4 * 0.5 = -0.2
-//     } else {
-//       // Shouldn't happen unless timer glitches
-//       leftFront.set(0);
-//       rightFront.set(0);
-//     }  
-//   }
-
-//   @Override
-//   public void teleopPeriodic() {
-//     // Original speed is based on controller input, so we multiply the input by 0.5
-//     m_robotDrive.arcadeDrive(m_controller.getLeftX() * 0.5, m_controller.getLeftY() * 0.5);
-//   }
-// }
-
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -84,6 +6,8 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
@@ -104,6 +28,13 @@ public class Robot extends TimedRobot {
   private final WPI_TalonSRX leftBack = new WPI_TalonSRX(3);
   private final WPI_TalonSRX rightBack = new WPI_TalonSRX(4);
 
+  // Double solenoid on PCM ports 1 (forward) and 2 (reverse)
+  private final DoubleSolenoid m_solenoid =
+      new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+
+  // Tracks current toggle state so we know which way to flip next
+  private boolean m_solenoidExtended = false;
+
   /** Called once at the beginning of the robot program. */
   public Robot() {
     // We need to invert one side of the drivetrain so that positive voltages
@@ -118,6 +49,9 @@ public class Robot extends TimedRobot {
 
     rightFront.setInverted(true);
     rightBack.setInverted(true);
+
+    // Start the solenoid in a known state (retracted)
+    m_solenoid.set(DoubleSolenoid.Value.kReverse);
   }
 
   @Override
@@ -140,7 +74,12 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    m_robotDrive.tankDrive(-m_controller.getLeftY() * 0.5, -m_controller.getRightY() * 0.5);  // tank drive mode
 
-      m_robotDrive.tankDrive(-m_controller.getLeftY() * 0.5, -m_controller.getRightY() * 0.5);  // tank drive mode
+    // Toggle the pneumatic in/out each time the A button is pressed
+    if (m_controller.getAButtonPressed()) {
+      m_solenoidExtended = !m_solenoidExtended;
+      m_solenoid.set(m_solenoidExtended ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse);
+    }
   }
 }
